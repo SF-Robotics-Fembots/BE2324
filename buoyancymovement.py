f7ba805910d8f512713b127324be4a3be19070a6
kinnera <25tirumala@stfrancishs.org>
2024-03-26 21:32:34 +0000

    adding files

--- buoyancymovement.py
+++ buoyancymovement.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import time
+import smbus
+import ms5837
 
 #initializing the variables
 motordown = 5
@@ -21,6 +23,34 @@ GPIO.setup(switch, GPIO.IN)
 GPIO.output(motorup, GPIO.LOW)
 GPIO.output(motordown, GPIO.LOW)
 
+def collect_pressure():
+    #initialize the sensor
+    print("initializing pressure sensor")
+    sensor.init()
+    time.sleep(1)
+    sensor.read(ms5837.OSR_256)
+    sensor.setFluidDensity(ms5837.DENSITY_FRESHWATER)
+    startup_success = 0 #set to false initially
+    while not startup_success:
+        try:
+            startup()
+        except:
+            print("failed startup")
+            pass
+        else:
+            startup_success = 1
+            time.sleep(0.5)
+    while True:
+        try:
+            sensor.read(ms5837.OSR_256)
+        except:
+            print("failed reading")
+            continue #goes back to the try. it's stubborn like that
+        readings = sensor.pressure(ms5837.UNITS_kPa)
+        now = datetime.datetime.now()
+        print(str(readings) + "   " + now.strftime("%H:%M:%S"))
+        break
+
 def plunger_up():
     while GPIO.input(switch) == True:
         GPIO.output(motorup, GPIO.HIGH)
@@ -41,6 +71,7 @@ def be_down():
     plunger_up()
     #not inclusive of the last second
     for descend_time in range(time_to_bottom):
+         collect_pressure()
          time.sleep(1)
 
 def be_up():
@@ -50,12 +81,11 @@ def be_up():
 
 def be_main():
      calibration()
-     while True:
-          be_down()
-          be_up()
+     
 
-def be_dive(event):
+def be_dive():
      print('running?')
+     
 
 
 #running main function
